a9df4f8d9307b7bef7435a8e371a5794ff7dd9fc
nor0x <nor0x@users.noreply.github.com>
2025-03-19 16:06:25 +0100

    download page

---
+++ Pages/Download.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GuckGuck_Web.Pages
+{
+    public class DownloadModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Add any logic here if needed in the future
+        }
+    }
+}
